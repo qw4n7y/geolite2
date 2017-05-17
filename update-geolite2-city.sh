06a30358320cf08b0ee24fbbcc23fd88970e8052
Yury Samarychau <qw4n7y@Yurys-MacBook-Pro.local>
2017-05-17 16:30:12 +0300

    stay on the edge!

--- update-geolite2-city.sh
+++ update-geolite2-city.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /usr/share/maxmind
 curl http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz > /usr/share/maxmind/GeoLite2-City.mmdb.gz
-gunzip /usr/share/maxmind/GeoLite2-City.mmdb.gz
+gunzip -f /usr/share/maxmind/GeoLite2-City.mmdb.gz
